509519803949336ab41518cbe02d948f29ed5b97
thiago mileo <thiago.mileo@grpnos.com>
2025-01-23 13:51:14 -0300

    correção de nomenclatura

--- internal/redis.go
+++ internal/redis.go
@@ -27,7 +27,7 @@ func GravarObjEmRedis(dados *Dados) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	compraJSON, err := json.Marshal(dados)
+	atenaObjJSON, err := json.Marshal(dados)
 	if err != nil {
 		return err
 	}
@@ -38,6 +38,6 @@ func GravarObjEmRedis(dados *Dados) error {
 		return err
 	}
 	expiracao := time.Duration(expireHours) * 24 * time.Hour
-	err = rdb.Set(ctx, dados.Processo, compraJSON, expiracao).Err()
+	err = rdb.Set(ctx, dados.Processo, atenaObjJSON, expiracao).Err()
 	return err
 }
